db32fd7fa860b58fb175808035a96b899d3023f1
awesomex <bestawes0mex@gmail.com>
2023-10-16 15:01:11 +0600

    book-library-app form

--- React-course/React-projects/30_book_library_app/frontend/src/components/BookForm/BookForm.jsx
+++ React-course/React-projects/30_book_library_app/frontend/src/components/BookForm/BookForm.jsx
@@ -1,9 +1,51 @@
 import './BookForm.css'
+import { useState } from 'react'
 
 const BookForm = () => {
+	const [date, setDate] = useState({
+		title: '',
+		author: '',
+	})
+
+	const handleSubmit = event => {
+		event.preventDefault()
+		if (date.author && date.title) {
+			//dispatch action
+
+			setDate({
+				title: '',
+				author: '',
+			})
+		}
+	}
 	return (
 		<div className='app-block book-form'>
 			<h2>Add new book</h2>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor='title'>
+						Title:
+						<input
+							type='text'
+							id='title'
+							value={date.title}
+							onChange={e => setDate({ ...date, title: e.target.value })}
+						/>
+					</label>
+				</div>
+				<div>
+					<label htmlFor='author'>
+						Author:
+						<input
+							type='text'
+							id='author'
+							value={date.author}
+							onChange={e => setDate({ ...date, author: e.target.value })}
+						/>
+					</label>
+				</div>
+				<button type='submit'>Add book</button>
+			</form>
 		</div>
 	)
 }
